ab56436aab26c52214d2972f6382f01cb009db45
Jorge Medrano <jamedranoa@unal.edu.co>
2014-06-27 15:58:22 -0400

    change seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,11 @@
+User.create!([
+  {email: "jorar91@gmail.com", password_digest: "$2a$10$lRyAQJZICyfUp5vv09lAQOWijYhQoX2yycmQhcnTtCMZBkXNzJVam", session_token: "BTicO8AJY7KDF8GOJZ-wfw", fname: "Teo", lname: "", education: "gives hand", job: "none", sex: "M", picture: "http://goo.gl/gCttuv"},
+  {email: "quira@gmail.com", password_digest: "$2a$10$5UmJ0go1gqkSOyCttaNpauUAi8VT.29Na4H2Ll.6i/SokcWBWZvTi", session_token: "a-g-pgV4vXecWpGJZEIHzA", fname: "quira", lname: "", education: nil, job: nil, sex: nil, picture: nil},
+  {email: "lolo@gmail.com", password_digest: "$2a$10$9QxAtW5iIO9/QwdVH8DCR.sIgct13G4At.xOEssCGlJREdHvp360e", session_token: "IMZ48vMEJCNp3G1gc7h7Vg", fname: "Lolo", lname: "", education: nil, job: nil, sex: nil, picture: "http://goo.gl/DegTzj"},
+  {email: "otherdog@gmail.com", password_digest: "$2a$10$TOLyroWXgfaWbrvxWOEeZetE0flmh0sPlbwnYC7xlew8b8xF0YYLm", session_token: "TivXryqkmkxZhx7iMMhmYg", fname: "other dog", lname: "", education: nil, job: nil, sex: nil, picture: "http://goo.gl/DegTzj"}
+])
+
+
 Place.create!([
   {owner_id: 1, title: "New York's House", kind: "", max_guest: 5, price: nil, bed_type: "", num_bathrooms: "4", num_beds: "", country: "", city: "New York", neighborhood: "", address: "", picture: "http://goo.gl/Mfmevh"},
   {owner_id: 1, title: "Chicago's House", kind: "nice", max_guest: 5, price: nil, bed_type: "", num_bathrooms: "", num_beds: "", country: "", city: "Chicago", neighborhood: "", address: "", picture: "http://goo.gl/6IajKV"},
@@ -5,9 +13,4 @@ Place.create!([
   {owner_id: 1, title: "Seattle's House", kind: "", max_guest: 5, price: 545, bed_type: "", num_bathrooms: "", num_beds: "", country: "", city: "Seattle", neighborhood: "", address: "", picture: "http://goo.gl/2W31Ot"},
   {owner_id: 1, title: "second new york house", kind: "", max_guest: nil, price: nil, bed_type: "", num_bathrooms: "", num_beds: "", country: "", city: "New York", neighborhood: "", address: "", picture: ""}
 ])
-User.create!([
-  {email: "quira@gmail.com", password_digest: "$2a$10$5UmJ0go1gqkSOyCttaNpauUAi8VT.29Na4H2Ll.6i/SokcWBWZvTi", session_token: "a-g-pgV4vXecWpGJZEIHzA", fname: "quira", lname: "", education: nil, job: nil, sex: nil, picture: nil},
-  {email: "jorar91@gmail.com", password_digest: "$2a$10$lRyAQJZICyfUp5vv09lAQOWijYhQoX2yycmQhcnTtCMZBkXNzJVam", session_token: "BTicO8AJY7KDF8GOJZ-wfw", fname: "Teo", lname: "", education: "gives hand", job: "none", sex: "M", picture: "http://goo.gl/gCttuv"},
-  {email: "lolo@gmail.com", password_digest: "$2a$10$9QxAtW5iIO9/QwdVH8DCR.sIgct13G4At.xOEssCGlJREdHvp360e", session_token: "IMZ48vMEJCNp3G1gc7h7Vg", fname: "Lolo", lname: "", education: nil, job: nil, sex: nil, picture: "http://goo.gl/DegTzj"},
-  {email: "otherdog@gmail.com", password_digest: "$2a$10$TOLyroWXgfaWbrvxWOEeZetE0flmh0sPlbwnYC7xlew8b8xF0YYLm", session_token: "TivXryqkmkxZhx7iMMhmYg", fname: "other dog", lname: "", education: nil, job: nil, sex: nil, picture: "http://goo.gl/DegTzj"}
-])
+
