2b5f98f8e9d7b19fc55b2adfd9bec9a8ca876f63
Jorge Medrano <jamedranoa@unal.edu.co>
2014-07-06 18:03:55 -0400

    more css

--- app/models/place_pic.rb
+++ app/models/place_pic.rb
@@ -1,5 +1,5 @@
 class PlacePic < ActiveRecord::Base
-  has_attached_file :picture, styles: {small: "150x150>", large: "300x300>"}, default_url:"/images/missing_house.jpg"
+  has_attached_file :picture, styles: {small: "150x150>", large: "1000x1000>"}, default_url:"/images/missing_house.jpg"
   #validates_attachment_presence :picture
   validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140705172945) do
+ActiveRecord::Schema.define(version: 20140706215247) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20140705172945) do
     t.string   "picture"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "description"
   end
 
   add_index "places", ["address"], name: "index_places_on_address", using: :btree
